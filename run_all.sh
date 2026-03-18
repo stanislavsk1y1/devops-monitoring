d574f797c27091b63e2d8f60c9666cd9e54217dc
Stanislav Barzykin <it.stanislavsk1y@atomicmail.io>
2026-03-18 22:50:48 +0300

    merge commit

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Запустить все проверки
 echo '=== Запуск мониторинга ==='
+echo "Запуск от пользователя: $(whoami)"
 echo "Время запуска: $(date)"
 bash check_disk.sh
 bash check_memory.sh
